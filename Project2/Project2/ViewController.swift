06a9fb7bcdabf033ef3cf6f5a93269af68a9e4b4
Liem Vo <liemvt@gmail.com>
2019-08-27 06:28:53 +1200

    Day 20

--- Project2/Project2/ViewController.swift
+++ Project2/Project2/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameKit
 
 class ViewController: UIViewController {
 
@@ -14,10 +15,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var button2: UIButton?
     @IBOutlet weak var button3: UIButton?
 	
-	var buttons = [UIButton?]()
+	private var buttons = [UIButton?]()
     
     private var countries = [String]()
     private var score = 0
+	
+	private var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,11 +37,32 @@ class ViewController: UIViewController {
 		}
 	}
 
-	private func askQuestion() {
+	private func askQuestion(action: UIAlertAction! = nil) {
+		correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+		countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
 		for (index, button) in [button1, button2, button3].enumerated() {
 			button?.setImage(UIImage(named: countries[index]), for: .normal)
 		}
+		title = countries[correctAnswer].uppercased()
 	}
-    
+	
+	
+	@IBAction func buttonTapped(_ sender: UIButton) {
+		var title: String
+		if (sender.tag == correctAnswer) {
+			score += 1
+			title = "Correct"
+		} else {
+			score -= 1
+			title = "Wrong"
+		}
+		showResult(title: title)
+	}
+	
+	private func showResult(title: String) {
+		let alertViewController = UIAlertController(title: title, message: "Your score is \(score).", preferredStyle: .alert)
+		alertViewController.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+		present(alertViewController, animated: true)
+	}
 }
 
