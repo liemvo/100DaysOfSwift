0c3000eb20528dc4a645c4a069a5b429aeebe0e8
Liem Vo <liemvt@gmail.com>
2019-09-02 19:19:33 +1200

    Day 28

--- Project5/Project5/ViewController.swift
+++ Project5/Project5/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+	
 	var allWords = [String]()
 	var usedWords = [String]()
 	
@@ -44,8 +44,60 @@ class ViewController: UITableViewController {
 	}
 	
 	private func submit(_ answer: String) {
+		let lowerAnswer = answer.lowercased()
+		var errorTitle: String = ""
+		var errorMessage: String = ""
+		if isPossible(word: lowerAnswer) {
+			if isOriginal(word: lowerAnswer) {
+				if isReal(word: lowerAnswer) {
+					usedWords.insert(answer, at: 0)
+					let indexPath = IndexPath(row: 0, section: 0)
+					tableView.insertRows(at: [indexPath], with: .automatic)
+				} else {
+					errorTitle = "Word not recognised"
+					errorMessage = "You can't just make them up, you know!"
+				}
+			} else {
+				errorTitle = "Word used already"
+				errorMessage = "Be more original!"
+			}
+		} else {
+			guard let title = title?.lowercased() else { return }
+			errorTitle = "Word not possible"
+			errorMessage = "You can't spell that word from \(title)"
+		}
+		
+		let ac = UIAlertController(title: errorTitle, message: errorMessage, preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .default))
+		present(ac, animated: true)
 	}
-
+	
+	private func isPossible(word: String) -> Bool {
+		guard var tempWord = title?.lowercased() else {
+			return false
+		}
+		
+		for letter in word {
+			if let position = tempWord.firstIndex(of: letter) {
+				tempWord.remove(at: position)
+			} else {
+				return false
+			}
+		}
+		return true
+	}
+	
+	private func isOriginal(word: String) -> Bool {
+		return !usedWords.contains(word)
+	}
+	
+	private func isReal(word: String) -> Bool {
+		let checker = UITextChecker()
+		let range = NSRange(location: 0, length: word.utf16.count)
+		let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+		return misspelledRange.location == NSNotFound
+	}
+	
 	private func loadWords() {
 		if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
 			if let startWords = try? String(contentsOf: startWordsURL) {
