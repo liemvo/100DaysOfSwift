9c2feae2c1733cdad9666cc4503777823451a61e
Liem Vo <liemvt@gmail.com>
2019-09-05 23:20:02 +1200

    Day 31 WIP

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -54,7 +54,28 @@ class ViewController: UIViewController {
 		for label in viewsDictionary.keys {
 			view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
 		}
-		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+	
+		let metrics = ["labelHeight": 88]
+//		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+		
+//		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
+		
+		var previous: UILabel?
+		for label in [label1, label2, label3, label4, label5] {
+			label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+			label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+			
+			if let previous = previous {
+				// we have a previous label – create a height constraint
+				label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+			} else {
+				label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+			}
+			
+			// set the previous label to be the current one, for the next loop iteration
+			previous = label
+		}
+
 	}
 
 
