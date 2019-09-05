84fb8c83100f51cea718ba4b66acfeba65c8d1cf
Liem Vo <liemvt@gmail.com>
2019-09-05 23:38:47 +1200

    Day 31 challenges

--- Project6b/Project6b/ViewController.swift
+++ Project6b/Project6b/ViewController.swift
@@ -55,21 +55,26 @@ class ViewController: UIViewController {
 			view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
 		}
 	
-		let metrics = ["labelHeight": 88]
+//		let metrics = ["labelHeight": 88]
 //		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight)]-[label2(labelHeight)]-[label3(labelHeight)]-[label4(labelHeight)]-[label5(labelHeight)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
 		
 //		view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]->=10-|", options: [], metrics: metrics, views: viewsDictionary))
 		
 		var previous: UILabel?
 		for label in [label1, label2, label3, label4, label5] {
-			label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-			label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+//			label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+			label.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+			label.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+			label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
 			
 			if let previous = previous {
 				// we have a previous label – create a height constraint
 				label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
 			} else {
 				label.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 0).isActive = true
+			}
+			if label == label5 {
+				label.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: 0).isActive = true
 			}
 			
 			// set the previous label to be the current one, for the next loop iteration
