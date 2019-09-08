cbb321c884689f816419267cd3701a9a719697ed
Liem Vo <liemvt@gmail.com>
2019-09-08 21:46:27 +1200

    Day 34

---
+++ Project7/Project7/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  Project7
+//
+//  Created by Liem Vo on 9/8/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+
+	var webView: WKWebView!
+	var detailItem: Petition?
+	
+	override func loadView() {
+		webView = WKWebView()
+		view = webView
+	}
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		showData()
+	}
+	
+	private func showData() {
+		guard let detailItem = detailItem else { return }
+		
+		let html = """
+		<html>
+		<head>
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<style> body { font-size: 150%; } </style>
+		</head>
+		<body>
+		\(detailItem.body)
+		</body>
+		</html>
+		"""
+		
+		webView.loadHTMLString(html, baseURL: nil)
+	}
+
+}
--- Project7/Project7/ViewController.swift
+++ Project7/Project7/ViewController.swift
@@ -11,17 +11,26 @@ import UIKit
 class ViewController: UITableViewController {
 	
 	var petitions = [Petition]()
+	
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		
-		let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+		let urlString: String
+		if navigationController?.tabBarItem.tag == 0 {
+			// urlString = "https://api.whitehouse.gov/v1/petitions.json?limit=100"
+			urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+		} else {
+			// urlString = "https://api.whitehouse.gov/v1/petitions.json?signatureCountFloor=10000&limit=100"
+			urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+		}
 		
 		if let url = URL(string: urlString) {
 			if let data = try? Data(contentsOf: url) {
 				parse(json: data)
+				return
 			}
 		}
+		showError()
 	}
 
 	private func parse(json: Data) {
@@ -31,6 +40,12 @@ class ViewController: UITableViewController {
 			petitions = jsonPetitions.results
 			tableView.reloadData()
 		}
+	}
+	
+	private func showError() {
+		let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+		ac.addAction(UIAlertAction(title: "OK", style: .default))
+		present(ac, animated: true)
 	}
 	
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -43,6 +58,12 @@ class ViewController: UITableViewController {
 		cell.textLabel?.text = petition.title
 		cell.detailTextLabel?.text = petition.body
 		return cell
+	}
+	
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+		let vc = DetailViewController()
+		vc.detailItem = petitions[indexPath.row]
+		navigationController?.pushViewController(vc, animated: true)
 	}
 }
 
