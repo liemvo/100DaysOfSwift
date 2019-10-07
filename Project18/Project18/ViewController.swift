6e890c9da17f03397f1330677d723a75c78bcdbf
Liem Vo <liemvt@gmail.com>
2019-10-07 23:15:02 +1300

    Day 64

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Liem Vo on 10/7/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+		print("I'm inside the viewDidLoad() method!")
+		
+		print(1, 2, 3, 4, 5, separator: "-")
+	}
+
+}
+
