bea6e36d010bd9db4c3fe659ee6893c281f28c2b
Liem Vo <liemvt@gmail.com>
2019-10-10 20:51:44 +1300

    Day 67 of #100DaysOfSwift

---
+++ Project19/Project19/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Project19
+//
+//  Created by Liem Vo on 10/10/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+	
+}
---
+++ Project19/Project19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19
+//
+//  Created by Liem Vo on 10/10/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+}
+
