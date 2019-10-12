22584fa7d2169ab476bde39ca505fd83300565ea
Liem Vo <liemvt@gmail.com>
2019-10-12 21:46:12 +1300

    Day 68 of #100DaysOfSwift

--- Project19/Extension/Action.js
+++ Project19/Extension/Action.js
@@ -2,12 +2,13 @@
 var Action = function() {}
 
 Action.prototype = {
-run: function(parameters) {
-	
-},
-finalize: function(parameters) {
-	
-}
+	run: function(parameters) {
+		parameters.completionFunction({"URL" : document.URL, "title": document.title });
+	},
+	finalize: function(parameters) {
+		var customJavaScript = parameters["customJavaScript"];
+		eval(customJavaScript);
+	}
 };
 
 var ExtensionPreprocessingJS = new Action
