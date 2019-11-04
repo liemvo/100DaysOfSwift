077f58e57e9b7ce6928ce719858f186445eee57b
Liem Vo <liemvt@gmail.com>
2019-11-04 22:05:07 +1300

    Day 83 of #100DaysOfSwift

---
+++ Project25/Project25/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Project25
+//
+//  Created by Liem Vo on 11/4/19.
+//  Copyright © 2019 Liem Vo. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+	var window: UIWindow?
+	
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
