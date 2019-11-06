c3498cbd2908908505437cdd10045215ce5c0a0a
Liem Vo <liemvt@gmail.com>
2019-11-07 08:42:28 +1300

    Day 86 of #100DaysOfSwift

--- Project26/Project26/GameScene.swift
+++ Project26/Project26/GameScene.swift
@@ -6,8 +6,8 @@
 //  Copyright © 2019 Liem Vo. All rights reserved.
 //
 
+import CoreMotion
 import SpriteKit
-import GameplayKit
 
 enum CollisionTypes: UInt32 {
 	case player = 1
@@ -17,7 +17,18 @@ enum CollisionTypes: UInt32 {
 	case finish = 16
 }
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
+	private var player: SKSpriteNode!
+	private var lastTouchPosition: CGPoint? = nil
+	private var motionManager: CMMotionManager?
+	private var isGameOver = false
+	
+	private var scoreLabel: SKLabelNode!
+	private var score = 0 {
+		didSet {
+			scoreLabel.text = "Score: \(score)"
+		}
+	}
 	
 	override func didMove(to view: SKView) {
 		let background = SKSpriteNode(imageNamed: "background")
@@ -25,7 +36,22 @@ class GameScene: SKScene {
 		background.blendMode = .replace
 		background.zPosition = -1
 		addChild(background)
+		
+		scoreLabel = SKLabelNode(fontNamed: "Chalkduster")
+		scoreLabel.text = "Score: 0"
+		scoreLabel.horizontalAlignmentMode = .left
+		scoreLabel.position = CGPoint(x: 16, y: 16)
+		scoreLabel.zPosition = 2
+		addChild(scoreLabel)
+		
 		loadLevel()
+		createPlayer()
+		
+		physicsWorld.gravity = .zero
+		physicsWorld.contactDelegate = self
+		
+		motionManager = CMMotionManager()
+		motionManager?.startAccelerometerUpdates()
 	}
 	
 	private func loadLevel() {
@@ -88,6 +114,84 @@ class GameScene: SKScene {
 					fatalError("Uknown level letter: \(letter)")
 				}
 			}
+		}
+	}
+	
+	private func createPlayer() {
+		player = SKSpriteNode(imageNamed: "player")
+		player.position = CGPoint(x:96, y: 672)
+		player.zPosition = 1
+		player.physicsBody = SKPhysicsBody(circleOfRadius: player.size.width / 2)
+		player.physicsBody?.allowsRotation = false
+		player.physicsBody?.linearDamping = 0.5
+		
+		player.physicsBody?.categoryBitMask = CollisionTypes.player.rawValue
+		player.physicsBody?.contactTestBitMask = CollisionTypes.star.rawValue | CollisionTypes.vortex.rawValue | CollisionTypes.finish.rawValue
+		
+		player.physicsBody?.collisionBitMask = CollisionTypes.wall.rawValue
+		addChild(player)
+	}
+	
+	override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else { return }
+		let location = touch.location(in: self)
+		lastTouchPosition = location
+	}
+	
+	override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+		guard let touch = touches.first else { return }
+		let location = touch.location(in: self)
+		lastTouchPosition = location
+	}
+	
+	override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+		lastTouchPosition = nil
+	}
+	
+	override func update(_ currentTime: TimeInterval) {
+		guard isGameOver == false else { return }
+		#if targetEnvironment(simulator)
+		if let lastTouchPosition = lastTouchPosition {
+			let diff = CGPoint(x: lastTouchPosition.x - player.position.x, y: lastTouchPosition.y - player.position.y)
+			physicsWorld.gravity = CGVector(dx: diff.x, dy: diff.y)
+		}
+		#else
+		if let accelerometerData = motionManager?.accelerometerData {
+			physicsWorld.gravity = CGVector(dx: accelerometerData.acceleration.y * -50, dy: accelerometerData.acceleration.x * 50)
+		}
+		#endif
+	}
+	
+	func didBegin(_ contact: SKPhysicsContact) {
+		guard let nodeA = contact.bodyA.node else { return }
+		guard let nodeB = contact.bodyB.node else { return }
+		
+		if nodeA == player {
+			playerCollided(with: nodeB)
+		} else if nodeB == player {
+			playerCollided(with: nodeA)
+		}
+	}
+	
+	private func playerCollided(with node: SKNode) {
+		if node.name == "vortex" {
+			player.physicsBody?.isDynamic = false
+			isGameOver = true
+			score -= 1
+			
+			let move = SKAction.move(to: node.position, duration: 0.25)
+			let scale = SKAction.scale(by: 0.0001, duration: 0.25)
+			let remove = SKAction.removeFromParent()
+			let sequence = SKAction.sequence([move, scale, remove])
+			player.run(sequence) { [weak self] in
+				self?.createPlayer()
+				self?.isGameOver = false
+			}
+		} else if node.name == "star" {
+			node.removeFromParent()
+			score += 1
+		} else if node.name == "finish" {
+			
 		}
 	}
 }
