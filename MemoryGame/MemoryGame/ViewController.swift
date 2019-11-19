f69ff7396fbd3f537cefc396199b2d44c2ed8a57
Liem Vo <liemvt@gmail.com>
2019-11-19 21:22:36 +1300

    Just complete day 99 and 100 of #100DaysOfSwift

--- MemoryGame/MemoryGame/ViewController.swift
+++ MemoryGame/MemoryGame/ViewController.swift
@@ -94,6 +94,7 @@ class ViewController: UIViewController {
 	@IBOutlet weak var label36: UILabel!
 	
 	private var buttons = [UIButton]()
+	private var labels = [UILabel]()
 	
 	private var countries = [Country]()
 	private var currentCountries = [Country]()
@@ -108,6 +109,15 @@ class ViewController: UIViewController {
 		
 		buttons = [button1, button2, button3, button4, button5, button6, button7, button8, button9, button10, button11, button12, button13, button14, button15, button16, button17, button18, button19, button20, button21, button22, button23, button24, button25, button26, button27, button28, button29, button30, button31, button32, button33, button34, button35, button36]
 		
+		labels = [label1, label2, label3, label4, label5, label6, label7, label8, label9, label10, label11, label12, label13, label14, label15, label16, label17, label18, label19, label20, label21, label22, label23, label24, label25, label26, label27, label28, label29, label30, label31, label32, label33, label34, label35, label36]
+		
+		labels.forEach {
+			$0.isHidden = true
+			$0.textAlignment = .center
+			$0.textColor = .white
+			$0.numberOfLines = 0
+		}
+		
 		for i in 0 ..< buttons.count {
 			let button = buttons[i]
 			button.tag = 100 + i
@@ -118,12 +128,28 @@ class ViewController: UIViewController {
 	
 	@IBAction func buttonTapped(_ sender: UIButton) {
 		let currentClick = sender.tag - 100
-		
+		perform(#selector(flip), with: (sender, labels[currentClick]), afterDelay: 0)
 		if previousClick != -1 {
+			let currentCountry = currentCountries[currentClick]
+			let previousCountry = currentCountries[previousClick]
 			
+			if currentCountry.capital == previousCountry.capital {
+				
+				// show previous card
+				perform(#selector(flip), with: (buttons[previousClick], labels[previousClick]), afterDelay: 0)
+				
+				
+				perform(#selector(hide), with: (currentClick, previousClick), afterDelay: 2)
+			} else {
+				perform(#selector(revert), with: (labels[currentClick], buttons[currentClick]), afterDelay: 2)
+			}
+			
+			previousClick = -1
 		} else {
 			previousClick = currentClick
+			perform(#selector(revert), with: (labels[currentClick], buttons[currentClick]), afterDelay: 2)
 		}
+		
 	}
 	
 	
@@ -161,14 +187,43 @@ class ViewController: UIViewController {
 			currentCountries = Array(countries.shuffled()[0..<buttons.count/2])
 			currentCountries += currentCountries
 			currentCountries.shuffle()
-			buttons.forEach { button in
-				button.isHidden = true
+			for i in 0 ..< currentCountries.count {
+				labels[i].text = currentCountries[i].capital
 			}
 		} else {
-			buttons.forEach { button in
-				button.isHidden = true
-			}
 			showError()
+		}
+	}
+	
+	@objc func flip(object: Any) {
+		if let (firstView, secondView) = object as? (UIView, UIView) {
+			let transitionOptions: UIView.AnimationOptions = [.transitionFlipFromRight, .showHideTransitionViews]
+			
+			UIView.transition(with: firstView, duration: 1.0, options: transitionOptions, animations: {
+				firstView.isHidden = true
+			})
+			
+			UIView.transition(with: secondView, duration: 1.0, options: transitionOptions, animations: {
+				secondView.isHidden = false
+			})
+			
+		}
+	}
+	
+	@objc func revert(object: Any) {
+		if let (firstView, secondView) = object as? (UIView, UIView) {
+			firstView.isHidden = true
+			secondView.isHidden = false
+		}
+	}
+	
+	@objc func hide(object: Any) {
+		if let (currentPosition, previousPosition) = object as? (Int, Int) {
+			labels[currentPosition].isHidden = true
+			labels[previousPosition].isHidden = true
+			
+			buttons[currentPosition].isHidden = true
+			buttons[previousPosition].isHidden = true
 		}
 	}
 }
